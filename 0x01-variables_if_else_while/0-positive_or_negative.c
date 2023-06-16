659ab98b76cdefc0c42388ef20947c833bdf997a
lamaa4 <44681715+lamaa4@users.noreply.github.com>
2023-06-16 18:16:21 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 if (n<0)
 {
-printf("is negative");
+printf("is negative \n");
 }
 else if (n>0)
 {
-printf("is positive");
+printf("is positive \n");
 }
 else
 {
-printf("is zero");
+printf("is zero \n");
 }
 return (0);
 }
