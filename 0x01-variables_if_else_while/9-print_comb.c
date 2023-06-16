ded41fa8d34e3d0ff344e5efb2d5706514f854b2
lamaa4 <44681715+lamaa4@users.noreply.github.com>
2023-06-16 20:05:54 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+{
+for (int num = 0; num < 10; num++){
+putchar(num+ '0');
+if (num==9)
+break;
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+ 
+
+return (0);
+}   
